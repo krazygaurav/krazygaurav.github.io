2215964f450c8da3750c6d45c9bc470b9bbd0f65
krazygaurav <singhal.gaurav93@gmail.com>
2018-06-02 21:38:04 +0530

    Bug Solving

--- js/main.js
+++ js/main.js
@@ -1,3 +1,151 @@
+
+
+/* Particle.js*/
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 88,
+            "density": {
+                "enable": true,
+                "value_area": 700
+            }
+        },
+        "color": {
+            "value": ["#aa73ff", "#f8c210", "#83d238", "#33b1f8"]
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 15
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1.5,
+                "opacity_min": 0.15,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 2.5,
+            "random": false,
+            "anim": {
+                "enable": true,
+                "speed": 2,
+                "size_min": 0.15,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 110,
+            "color": "#33b1f8",
+            "opacity": 0.25,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 1.6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": false,
+                "mode": "repulse"
+            },
+            "onclick": {
+                "enable": false,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+});
+
+/* Typing.js */
+$(function(){
+	$(".typed").typed({
+		strings: ["Tech Agnostic", "Consultant", "Web Dev", "Mentor"],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 30,
+		// time before typing starts
+		startDelay: 1200,
+		// backspacing speed
+		backSpeed: 20,
+		// time before backspacing
+		backDelay: 500,
+		// loop
+		loop: true,
+		// false = infinite
+		loopCount: 5,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+	});
+});
+
+
+/* Main.js */
 document.addEventListener("DOMContentLoaded", function () {
     new SweetScroll({});
     particlesJS("particles-js", {
@@ -132,6 +280,7 @@ function manageProjects(hide, doHide){
     });
 }
 $(document).ready(function () {
+    $('body').toggleClass('loaded');
     document.addEventListener('DOMContentLoaded', () => {
         const scroller = new SweetScroll({});
     }, false);
